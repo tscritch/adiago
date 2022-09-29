b353fc48308accd593310a48ada2ee58c309e43f
Tad Scritchfield <tadscritch@gmail.com>
2022-09-28 22:57:27 -0500

    fix formatTime function

--- components/src/_utils/functions/index.ts
+++ components/src/_utils/functions/index.ts
@@ -3,7 +3,8 @@ export const formatTime = (time: number): string => {
   const hours = date.getUTCHours();
   const minutes = date.getMinutes();
   const seconds = date.getSeconds();
-  return `${hours === 0 ? (hours < 10 ? '0' + hours : hours) + ':' : ''}${minutes < 10 ? '0' + minutes : minutes}:${
+
+  return `${hours === 0 ? '' : (hours < 10 ? '0' + hours : hours) + ':'}${minutes < 10 ? '0' + minutes : minutes}:${
     seconds < 10 ? '0' + seconds : seconds
   }`;
 };
